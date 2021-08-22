aa2228807979ba1f113ebab620a4ce326b4731e9
Oksana Repchak <oksa.repchak@gmail.com>
2021-08-22 23:00:29 +0300

    fix

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const backdrop = document.querySelector("[data-backdrop]");
 
+  // const refs = {
+  //   openModalBtn: document.querySelector("[data-open-modal]"),
+  //   closeModalBtn: document.querySelector("[data-close-modal]"),
+  //   backdrop: document.querySelector("[data-backdrop]"),
+  // };
+
+  // refs.openModalBtn.addEventListener("click", toggleModal);
+  // refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  // function toggleModal() {
+  //   backdrop.classList.toggle("is-hidden");
+  // }
+
+  // --------------------
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -10,5 +25,7 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+
+    backdrop.classList.toggle("is-hidden");
   });
 })();
